8dda1962e86f9ee784a1f04b9078161f4a0245d4
Alex Martín <amartinj@gmail.com>
2017-06-30 10:23:59 +0000

    Fix migration

---
+++ db/migrate/20170629111105_create_user_table_tokens.rb
@@ -0,0 +1,21 @@
+require 'carto/db/migration_helper'
+
+include Carto::Db::MigrationHelper
+
+migration(
+    Proc.new do
+      create_table :user_table_tokens do
+        Uuid        :id, primary_key: true, default: 'uuid_generate_v4()'.lit
+        foreign_key :table_id, :user_tables, null: false, type: :uuid, on_delete: :cascade
+        Boolean     :write_access, null: false, default: false
+        String      :value
+        index [:value], :unique =>true
+      end
+    end,
+    Proc.new do
+      drop_table :user_table_tokens
+    end
+)
+
+
+
