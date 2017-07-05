fe6be52b287c81f1f9d818e8b3976b0efd013546
Alex Martín <amartinj@gmail.com>
2017-07-05 11:16:28 +0200

    Generate token value based on user's salt

--- app/models/carto/user_table_token.rb
+++ app/models/carto/user_table_token.rb
@@ -1,8 +1,14 @@
 require 'active_record'
+require 'digest'
+require 'date'
 require_dependency 'carto/db/sanitize'
 
 module Carto
   class UserTableToken < ActiveRecord::Base
     belongs_to :table, class_name: Carto::UserTable, inverse_of: :tokens
+
+    def generate_value!
+      update_attribute(:value, Digest::SHA1.hexdigest("#{table.user.salt}:#{DateTime.now.strftime('%Q')}"))
+    end
   end
 end
