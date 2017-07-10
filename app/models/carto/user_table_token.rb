68b31c59f7ae22edac9ec6dcba17432c4c29500c
Alex Martín <amartinj@gmail.com>
2017-07-10 12:55:26 +0000

    Make sure there are no collisions for creating tokens for the same user in the same microsecond

--- app/models/carto/user_table_token.rb
+++ app/models/carto/user_table_token.rb
@@ -8,7 +8,8 @@ module Carto
     belongs_to :table, class_name: Carto::UserTable, inverse_of: :tokens
 
     def generate_value!
-      update_attribute(:value, Digest::SHA1.hexdigest("#{table.user.salt}:#{DateTime.now.strftime('%Q')}"))
+      rnd = Random.new()
+      update_attribute(:value, Digest::SHA1.hexdigest("#{table.user.salt}:#{rnd.rand(DateTime.now.strftime('%Q').to_i)}"))
     end
   end
 end
